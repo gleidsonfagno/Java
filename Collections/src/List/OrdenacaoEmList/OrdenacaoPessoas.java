4c82c17522e932b190b4e7b3dbfed8e014813c04
gleidsonfagno <gleidsonfagner838@gmail.com>
2026-02-19 21:09:00 -0300

    Ordenação em List

---
+++ Collections/src/List/OrdenacaoEmList/OrdenacaoPessoas.java
@@ -0,0 +1,39 @@
+package List.OrdenacaoEmList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add( new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        pessoasPorAltura.sort(new CompararPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacao = new OrdenacaoPessoas();
+        ordenacao.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacao.adicionarPessoa("Nome 2", 30, 1.46);
+        ordenacao.adicionarPessoa("Nome 3", 25, 1.86);
+
+        System.out.println("Pessoas ordenadas por idade:" + ordenacao.ordenarPorIdade());
+        System.out.println(ordenacao.ordenarPorAltura());
+    }
+}
---
+++ Collections/src/List/OrdenacaoEmList/Pessoa.java
@@ -0,0 +1,49 @@
+package List.OrdenacaoEmList;
+
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa> {
+
+    private  String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(this.idade, p.idade);
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                '}';
+    }
+}
+
+class CompararPorAltura implements Comparator<Pessoa> {
+
+    @Override
+    public int compare(Pessoa pessoa1, Pessoa pessoa2) {
+        return Double.compare(pessoa1.getAltura(), pessoa2.getAltura());
+    }
+}
