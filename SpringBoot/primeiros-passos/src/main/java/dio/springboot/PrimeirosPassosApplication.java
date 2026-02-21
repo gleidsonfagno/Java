da3490cce47824fa6045972012a7913dd0188f5e
gleidsonfagno <gleidsonfagner838@gmail.com>
2026-02-21 15:48:22 -0300

    Bean e Component

--- SpringBoot/primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ SpringBoot/primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -1,7 +1,10 @@
 package dio.springboot;
 
+import dio.springboot.Cep.ConversorJson;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
@@ -9,6 +12,15 @@ public class PrimeirosPassosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
 
+	}
+
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			dio.springboot.Cep.ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
 	}
 
 }
