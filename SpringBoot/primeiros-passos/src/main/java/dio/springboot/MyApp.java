362c860029387cfc8b2943786b6c3a28541c34fb
gleidsonfagno <gleidsonfagner838@gmail.com>
2026-02-21 14:42:24 -0300

    Primeiros passos

--- SpringBoot/primeiros-passos/src/main/java/dio/springboot/MyApp.java
+++ SpringBoot/primeiros-passos/src/main/java/dio/springboot/MyApp.java
@@ -1,4 +1,16 @@
 package dio.springboot;
 
-public class MyApp {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApp implements CommandLineRunner {
+    @Autowired
+    private Calculadora calculadora;
+    @Override
+    public void run(String... args) throws Exception {
+        int resultado = calculadora.somar(1, 2);
+        System.out.println("O resultado : " + resultado);
+    }
 }
