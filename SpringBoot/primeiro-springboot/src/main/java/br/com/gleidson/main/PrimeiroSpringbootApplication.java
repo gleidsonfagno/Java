7af4952e4a202077c1c36e9b4759b82249098dd9
gleidsonfagno <gleidsonfagner838@gmail.com>
2026-03-17 20:24:18 -0300

    Setup do project

---
+++ SpringBoot/primeiro-springboot/src/main/java/br/com/gleidson/main/PrimeiroSpringbootApplication.java
@@ -0,0 +1,14 @@
+package br.com.gleidson.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroSpringbootApplication.class, args);
+	}
+
+}
+  
