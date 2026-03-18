4c15114b42cd0ca0f8ee2e172012e75e47282e52
gleidsonfagno <gleidsonfagner838@gmail.com>
2026-03-17 21:25:50 -0300

    Component Scan

--- SpringBoot/primeiro-springboot/src/main/java/br/com/gleidson/main/primeirospringbootaplcation/PrimeiroSpringbootApplication.java
+++ SpringBoot/primeiro-springboot/src/main/java/br/com/gleidson/main/primeirospringbootaplcation/PrimeiroSpringbootApplication.java
@@ -1,12 +1,14 @@
 package br.com.gleidson.main.primeirospringbootaplcation;
 
+import br.com.gleidson.main.controller.PrimeiraController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-
+@ComponentScan(basePackages = "br.com.gleidson.main")
+//@ComponentScan(basePackageClasses = PrimeiraController.class)
 public class PrimeiroSpringbootApplication {
 
 	public static void main(String[] args) {
